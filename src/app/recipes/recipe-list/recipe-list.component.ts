450fb18dd5592c9a064a2e6130f68ea41190f088
prakumar62 <prakumar62@publicisgroupe.net>
2019-11-15 07:50:54 +0530

    Chapter 4 completed

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    // tslint:disable-next-line: max-line-length
+    new Recipe('A test Recipe 1', 'This is a test recipe', 'https://commons.wikimedia.org/wiki/File:Recipe_logo.jpeg'),
+    new Recipe('A test Recipe 2', 'This is a test recipe', 'https://commons.wikimedia.org/wiki/File:Recipe_logo.jpeg')
+  ];
   constructor() { }
 
   ngOnInit() {
